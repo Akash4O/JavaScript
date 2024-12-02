7455a82978777c927d05be047a9ecf037795ee03
Nullbaba <akashkolde1320@gmail.com>
2024-12-02 12:52:01 +0000

    Datatype conversion

---
+++ 01_Basics/03_ConversionOperation.js
@@ -0,0 +1,36 @@
+// let score = "33abc"
+
+// console.log(typeof score);
+
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//Conversion to Number
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = "akash"
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+
+console.log(typeof booleanisLoggedIn);
+console.log(booleanisLoggedIn);
+
+
+//Boolean conversion
+// "" => false
+// 0 => false ; 1 => true
+// "Akash" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
