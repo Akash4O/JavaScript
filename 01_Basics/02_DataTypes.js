884ca03ea3eea7d349b3a7b06e842d5bcf891775
Nullbaba <akashkolde1320@gmail.com>
2024-12-02 12:25:26 +0000

    Data types

--- 01_Basics/02_DataTypes.js
+++ 01_Basics/02_DataTypes.js
@@ -1,1 +1,31 @@
 "use strict"; //treat all JS code as newer version
+
+// alert(3+3) // we are using node.js, not browser 
+
+console.log(3 + 3); //write code in a format where code readability should be high
+console.log("Akash");
+
+
+let name = "Akash" //String DataType
+let age = 18 //Number DataType
+let isLoggedIn = false //Boolean DataType
+
+// number => 2^53 range
+// bigint used for bigger numbers than int/number
+
+// string => sequence of characters enclosed within ""
+
+// boolean => true or false
+
+// null => standalone value means it is empty variable and type of null is an object
+
+//undefined => a variable which is not initialized
+
+//symbol => these are the data types which are used for specifing unique type mostly used in figma,react.js
+
+// object => an instance of classs
+
+//typeof function return which data type of a value is
+
+console.log(typeof null); // Object
+console.log(typeof undefined); //undefined
