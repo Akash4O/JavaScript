2cbc7cb755a3a60f46be2f065c77e54c5890f108
Nullbaba <akashkolde1320@gmail.com>
2024-12-03 12:35:26 +0000

    Operations

--- 01_Basics/03_ConversionOperation.js
+++ 01_Basics/03_ConversionOperation.js
@@ -14,23 +14,64 @@
 // null => 0
 // undefined => NaN
 
-let isLoggedIn = "akash"
+// let isLoggedIn = "akash"
 
-let booleanisLoggedIn = Boolean(isLoggedIn)
+// let booleanisLoggedIn = Boolean(isLoggedIn)
 
-console.log(typeof booleanisLoggedIn);
-console.log(booleanisLoggedIn);
-
-
-//Boolean conversion
-// "" => false
-// 0 => false ; 1 => true
-// "Akash" => true
+// console.log(typeof booleanisLoggedIn);
+// console.log(booleanisLoggedIn);
 
 
-let someNumber = 33
+// //Boolean conversion
+// // "" => false
+// // 0 => false ; 1 => true
+// // "Akash" => true
 
-let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33
+
+// let stringNumber = String(someNumber)
+
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+// *********************** Operations ***********************
+
+let value =3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "hello"
+// let str2 = " Akash"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3+4)*5%3);
+
+
+// console.log(+true);
+// console.log(+"");
+
+let num1 , num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gamecounter = 100
+++gamecounter;
+console.log(gamecounter);
